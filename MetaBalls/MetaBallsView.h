8fa085fbf4c5038e3176e15e72aba4b2ba706822
ParkerHitch <parker.hitchcock@gmail.com>
2023-05-10 20:24:55 -0500

    Initial Commit

---
+++ MetaBalls/MetaBallsView.h
@@ -0,0 +1,12 @@
+//
+//  MetaBallsView.h
+//  MetaBalls
+//
+//  Created by Parker Hitchcock on 5/10/23.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MetaBallsView : ScreenSaverView
+
+@end
